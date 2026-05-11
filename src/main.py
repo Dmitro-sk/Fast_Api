63d968e407264faf6b1ca6779a183dabff931e71
Dmitro-sk <copicdmitro@gmail.com>
2026-05-11 15:12:20 +0200

    dd entry point to main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,3 +6,5 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"status": "Lab 1 ", "message": "Hello"}
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
